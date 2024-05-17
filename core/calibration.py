746a6a55c201ce14d8b570fb7efb070d1503caf1
Maxim Degi <unliex@yandex.ru>
2024-05-17 09:13:40 +0300

    Corrected sampling frequency and color issue in draw_text_in_the_middle. Made fixed epoch time to 0.5, customising is planned. Edited calibration logic, working on correctly displaying completed morse code in it.

--- core/calibration.py
+++ core/calibration.py
@@ -6,8 +6,10 @@ from neurosdk.cmn_types import SensorFamily, SensorCommand
 import numpy as np
 import pygame
 import matplotlib.pyplot as plt
+import random
 import sys
 import time
+import datetime
 
 ######## it allows to save incoming signal right in the Sensor instance
 Sensor.data = []
@@ -16,14 +18,14 @@ def draw_text_in_the_middle(text: str,
                             color: pygame.Color | tuple[int,int,int],
                             screen: pygame.Surface, 
                             font: pygame.font.Font) -> None:
-    """Note: newlines supported, although pygame doesn't"""
+    """Note: newlines not ignored"""
 
     text_lines = text.split('\n')
     WIDTH, HEIGHT = screen.get_size()
     for i, line in enumerate(text_lines):
         line_size = font.size(line)
         screen.blit(
-            font.render(line, True, WHITE),
+            font.render(line, True, color),
             ((WIDTH - line_size[0])//2, (HEIGHT + line_size[1] * i * 2)//2)
             )
         
@@ -37,7 +39,7 @@ sensor.connect()
 sensor.exec_command(SensorCommand.StartSignal)
 
 WIDTH, HEIGHT = (1200, 800)
-SAMPLING_FREQUENCY = 9600
+SAMPLING_FREQUENCY = 1000
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
@@ -69,8 +71,16 @@ while greeting:
         elif event.type == pygame.KEYDOWN:
             greeting = False
 
+
+##### Setting speed #####
+epoch_time = 0.5
+epoch_size = epoch_time * SAMPLING_FREQUENCY
+#TODO: Make changing symbol typing speed available. 
+#FIXME: indicating speed requires checking exact time, that python can't handle fast
+
+
 ###### Getting noise value ######
-NO_BLINKING_TIME = 7
+NO_BLINKING_TIME = 0
 PREPARING_FOR_NO_BLINKING_TEXT = (f"Вам нужно будет не моргать и сидеть спокойно {NO_BLINKING_TIME} секунд.\n "
                                    "Как будете готовы, нажмите любую клавишу.")
 NO_BLINKING_TEXT = ("Не моргайте. Осталось time_left секунд..")
@@ -102,120 +112,48 @@ while time_left > 0:
 noise_value = np.max(noise_data)
 #TODO: Check that user really was calm by standard deviation or something
 
-###### Getting thresholds ######
-PREPARING_FOR_CALIBRATION_TEXT = "Далее вам нужно будет моргать для обозначения точек или тире. \n" \
-                            "Как будете готовы, нажмите любую клавишу."
-PROMPTING_DOT_BLINK_TEXT = "." + '\n'*7 + '(точка)'
-PROMPTING_DASH_BLINK_TEXT = "-" + '\n'*7 + '(тире)'
-USER_ACCURACY = ("Ваша погрешность моргания для точки: dot_blink_error.\n"
-                "Для тире: dash_blink_error\n"
-                "Чтобы продолжить, нажмите Пробел.\n"
-                "Чтобы завершить, нажмите Escape.")
-USER_HIGH_ACCURACY_REPORT = ("Погрешность моргания достаточно низкая!\n"
-                             "Вы можете завершить калибровку")
-USER_LOW_ACCURACY_REPORT = ("Погрешность моргания пока высокая."
-                            "Продолжайте калибровку.")
-
-OPTIMAL_DEVIATION = 30 # optimal difference between two epoch threshold. low deviation shows that user is blinking with persistently same values
-OPTIMAL_ITERATIONS = 5 # user will likely start giving persistent blinks within this iteration amount
-ITERATION_SIZE = 10 # amount of prompted dash or dots within an iteration
-MIN_PROMPT_INTERVAL= 0.5
-MAX_PROMPT_INTERVAL = 2
-
+##### Getting thresholds on generated data #####
 #preparing
+PREPARING_FOR_CALIBRATING_TEXT = "Далее вам будет дана последовательность символов, \nкоторые вам нужно будет ввести.\n" \
+                                "Постарайтесь моргать с одинаковой силой для каждого типа символа."
 screen.fill(BLACK)
-draw_text_in_the_middle(PREPARING_FOR_CALIBRATION_TEXT, WHITE, screen, font)
+draw_text_in_the_middle(PREPARING_FOR_CALIBRATING_TEXT, WHITE, screen, font)
 pygame.display.flip()
 
-preparing_for_calibration = True
-while preparing_for_calibration:
+preparing_for_calibrating = True
+while preparing_for_calibrating:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            preparing_for_calibration = False
+            preparing_for_calibrating = False
 
-dot_threshold = 0
-dash_threshold = 0
 
-iteration = 1
-#fill with garbage to avoid errors with computing deviation on first iteration
-dot_threshold_peaks = [1000]
-dash_threshold_peaks = [1000]
+#getting thresholds
+high_threshold = 0
+low_threshold = 0
+TEST_DATA_LENGTH = 15
+test_morse: str = ''.join([random.choice(('*','-',' ')) for _ in range(TEST_DATA_LENGTH)])
 
-dot_blink_error = 1000
-dash_blink_error = 1000
+screen.fill(ORANGE)
+draw_text_in_the_middle(''.join(test_morse), WHITE, screen, font)
+pygame.display.flip()
 
-#calibrating
-calibrating = True
-while calibrating:
-    for epoch in range(ITERATION_SIZE):
-        epoch_data = []
-        mode = np.random.random() < 0.5 # prompting dot or dash
-        text = PROMPTING_DOT_BLINK_TEXT if mode == 0 else PROMPTING_DASH_BLINK_TEXT
-        screen.fill(ORANGE)
-        draw_text_in_the_middle(text, BLACK, screen, font)
-        #make a line that appears and dissapears instantly for user to react
-        pygame.display.flip()
-        interval = np.random.uniform(MIN_PROMPT_INTERVAL, MAX_PROMPT_INTERVAL) # for user to get persistent in different intervals between blinks.
-
-        start = time.time()
-        end = start + interval
-        time_left = interval
-        while time_left > 0:
-            epoch_data.extend(sensor.data)
-            time_left = end - time.time()
-        (dot_threshold_peaks if mode == 0 else dash_threshold_peaks).append(np.max(epoch_data)) 
-
-        screen.fill(ORANGE) # for message to dissapear. it will make user react on next message better
-        pygame.display.flip()
-
-    screen.fill(BLUE)
-
-    last_dot_blink_error = dot_blink_error
-    last_dash_blink_error = dash_blink_error
-    dot_blink_error = np.round(np.std(dot_threshold_peaks),2)
-    dash_blink_error = np.round(np.std(dash_threshold_peaks),2)
-    dot_deviation = np.abs(dot_threshold_peaks[-2] - dot_threshold_peaks[-1])
-    dash_deviation = np.abs(last_dash_blink_error - dash_blink_error)
-
-    accuracy_report = (USER_HIGH_ACCURACY_REPORT 
-                       if dot_deviation < OPTIMAL_DEVIATION and dash_deviation < OPTIMAL_DEVIATION
-                  else USER_LOW_ACCURACY_REPORT)
-
-    dot_threshold = np.mean(dot_threshold_peaks)
-    dash_threshold = np.mean(dash_threshold_peaks)
-
-    #persistence of user blink strength
-    #NOTE: accuracy is counted on all calibration epochs, not current one
-    print('peaks',dot_threshold_peaks, dash_threshold_peaks)
-    print()
-    draw_text_in_the_middle(
-        USER_ACCURACY.replace("dot_blink_error", str(dot_deviation)).replace("dash_blink_error", str(dash_deviation)) + '\n' + accuracy_report,
-        WHITE,
-        screen,
-        font
-        ) 
+completed_morse: str = ''
+epoch_signals: list[list[float]] = []
+epoch: list[float] = []
+for symbol in test_morse:
+    start = time.time() + datetime.datetime.now().microsecond/1e6
+    while time.time() + datetime.datetime.now().microsecond/1e6 < start + epoch_time:
+        epoch.extend(sensor.data)
+        ...
+    completed_morse += symbol
+    draw_text_in_the_middle(''.join(test_morse), WHITE, screen, font)
+    draw_text_in_the_middle(completed_morse + '_' * (len(test_morse) - len(completed_morse)), # make completed symbols be green
+                            GREEN, screen, font) 
     pygame.display.flip()
-
-    waiting_to_proceed = True
-    while waiting_to_proceed:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit(0)
-            elif event.type == pygame.KEYDOWN:
-                match event.key:
-                    case pygame.K_ESCAPE:
-                        waiting_to_proceed = False
-                        calibrating = False
-                    case pygame.K_SPACE:
-                        waiting_to_proceed = False
-                         
-
-    iteration += 1
-
+    epoch_signals.append(epoch)
 del scanner
 del sensor
 pygame.quit()
